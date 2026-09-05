63c6090bea808c65699897e9adbb3475f2280e8e
CodeXDev-sg <itissausage67@gmail.com>
2026-09-05 12:09:33 +0500

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,11 +1,8 @@
 nasm -f bin boot.asm -o boot.bin
 nasm -f elf32 kernel_entry.asm -o kernel_entry.o
-#i686-elf-gcc -ffreestanding -nostdlib -m16 -c proto.c -o pmain.o
 i686-elf-g++ -fno-leading-underscore -mno-80387 -mno-fp-ret-in-387 -mno-mmx -mno-sse -mno-sse2 -O2 -ffreestanding -fno-pie -nostdlib -m16 -c kernel.c -o kernel.o
 i686-elf-ld -T linker.ld -o KERNEL.BIN kernel_entry.o kernel.o --oformat binary
-#kernel_entry.o
 dd if=/dev/zero of=boot.img bs=512 count=2880 conv=notrunc status=none
 mkfs.vfat boot.img -F 12 -n "PlanetOS"
 dd status=none if=boot.bin of=boot.img conv=notrunc
 mcopy -i boot.img KERNEL.BIN ::/
-mcopy -i boot.img START.SYS ::/
